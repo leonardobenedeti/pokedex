cc14373098c1baa3f79fd22e8011cf3a2a19da97
Leonardo Benedeti <leonardobenedeti@gmail.com>
2026-01-12 16:00:45 -0300

    chore: change icon and splashicon, add constants to assets and colors

---
+++ lib/core/constants/assets_constants.dart
@@ -0,0 +1,5 @@
+class AssetsConstants {
+  static const String _imagesPath = 'assets/images';
+
+  static const String logo = '$_imagesPath/pokeball.png';
+}
---
+++ lib/core/constants/colors_constants.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class ColorsConstants {
+  static const Color primary = Color(0xFFD30A40);
+  static const Color white = Colors.white;
+}
